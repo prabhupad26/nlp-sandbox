bdfc1967e6e2a8cfb734eb98601619cfbe5fbb04
prabhupad26 <prabhupad26@gmail.com>
2021-04-27 10:15:07 +0530

    Fix for DevToolsActivePort file doesn't exist issue

--- language_classifier/scrape_data.py
+++ language_classifier/scrape_data.py
@@ -28,6 +28,13 @@ if __name__ == '__main__':
     chromeOptions = webdriver.ChromeOptions()
     chromeOptions.add_argument("--remote-debugging-port=9222")
     chromeOptions.add_argument("--headless")
+    chromeOptions.add_argument("--no-sandbox")
+    chromeOptions.add_argument("--disable-setuid-sandbox")
+    chromeOptions.add_argument("--disable-dev-shm-using")
+    chromeOptions.add_argument("--disable-extensions")
+    chromeOptions.add_argument("--disable-gpu")
+    chromeOptions.add_argument("start-maximized")
+    chromeOptions.add_argument("disable-infobars")
     try:
         driver.get("http://www.python.org")
         assert "Python" in driver.title
